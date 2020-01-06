b4e02b00667871d8c9ba4a0965b83c1684dcfcaf
Andrea Fortuna <andrea@andreafortuna.org>
2020-01-06 21:39:10 +0100

    first

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function (req, res) {
+  //res.send('Got a POST request')
+  res.render('stream', { title: 'Streaming...', "magnet": req.body.magnet });
+})
+
+module.exports = router;
