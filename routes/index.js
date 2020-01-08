65311d2b3057d88167732ddf312d520131440c61
andrea.fortuna <andrea.fortuna@fortunaa-osx-sub.local>
2020-01-08 16:34:49 +0100

    new flow

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 let fs = require("fs")
   
-var WebTorrent = require('webtorrent-hybrid')
+var WebTorrent = require('webtorrent')
 
 let client = new WebTorrent()
 
